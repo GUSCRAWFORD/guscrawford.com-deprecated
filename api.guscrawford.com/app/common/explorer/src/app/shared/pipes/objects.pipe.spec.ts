ebe4e766b9cb16a2fbf8bf9d72a479847a328e3a
Gus Crawford <gus.crawford@envisageny.com>
2018-02-05 10:22:42 -0500

    dump old explorer app

--- api.guscrawford.com/app/common/explorer/src/app/shared/pipes/objects.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ObjectsPipe } from './objects.pipe';
-
-describe('ObjectsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ObjectsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
