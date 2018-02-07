e1473f2487aa1b676924643374cc70da76c8878e
Gus Crawford <gus.crawford@envisageny.com>
2018-02-07 11:26:47 -0500

    add passport, use basic auth with api

--- api.guscrawford.com/app/GusCrawfordDotComApp.ts
+++ api.guscrawford.com/app/GusCrawfordDotComApp.ts
@@ -1,6 +1,8 @@
 import {
     ODataServer,
-    odata, ODataController, Edm
+    odata, ODataController, Edm,
+    HttpRequestError,
+    ODataErrorHandler
 } from "odata-v4-server";
 import { UsersController} from './controllers/UsersController';
 import { PostsController } from './controllers/PostsController';
@@ -42,6 +44,24 @@ export class GusCrawfordDotComApp extends ODataServer {
 //GusCrawfordDotComApp.create('/'+APP_CONFIG[ENV].prefix, APP_CONFIG[ENV].port);
 
 import * as express from 'express';
+import * as passport from 'passport';
+
+import { BasicStrategy } from 'passport-http';
+import { STATUS_CODES } from 'http'
+class AuthenticationError extends HttpRequestError{
+    constructor(){
+        super(401, STATUS_CODES[401]);
+    }
+}
+passport.use(new BasicStrategy((userid:string, password:string, done:(error:any, user?:any) => void) => {
+    if (userid == "admin" && password == "admin") return done(null, "admin");
+    done(new AuthenticationError());
+}));
 const app = express();
-app.use('/v1', GusCrawfordDotComApp.create())
+app.use(
+    '/v1',
+    passport.authenticate("basic", { session: false, failWithError: true }),
+    GusCrawfordDotComApp.create(),
+    ODataErrorHandler
+);
 app.listen(APP_CONFIG[ENV].port)
