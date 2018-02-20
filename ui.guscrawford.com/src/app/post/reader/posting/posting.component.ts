282b91185866cf4c3a32b6f3500b293f9278ba7c
Gus Crawford <gus.crawford@envisageny.com>
2018-02-20 03:15:24 -0500

    switch markdown editors

--- ui.guscrawford.com/src/app/post/reader/posting/posting.component.ts
+++ ui.guscrawford.com/src/app/post/reader/posting/posting.component.ts
@@ -38,14 +38,14 @@ export class PostingComponent implements OnInit, DoCheck {
     this.model.post = val;
     if (val && val.content) {
       let imageRegExp = /!\[(.*)\]\((.*)\)/g,
-          titles = val.content.match(/\#.*/g),
+          titles = val.content.match(/\#\s.*/g),
           images = val.content.match(imageRegExp),
           imageless = val.content.replace(imageRegExp,''),
           imagelessLines = imageless.match(/.*/g).filter(m=>!!m),
-          preview = imagelessLines?imagelessLines.slice(0,MAX_PREVIEW_LINES).join('\n\n'):'',
+          preview = imagelessLines?imagelessLines.slice(0,MAX_PREVIEW_LINES).join('\n\n').replace(/\|/g,' '):'',
           imageAlt = images && images[0] && images[0].match(/!\[.*\]/),
           image = images && images[0] && images[0].match(/\(.*\)/);
-      if (titles && titles.length) this.onTitleChange.emit(titles[0].substring(1));
+      if (titles && titles.length) this.onTitleChange.emit(titles[0].substring(1).trim());
       if (image && image.length) this.onImageChange.emit(image[0].substr(1,image[0].length-2));
       if (imageAlt && imageAlt.length) this.onImageAltChange.emit(imageAlt[0].substr(2,imageAlt[0].length-3));
       this.onPreviewChange.emit(preview.length>MAX_PREVIEW_CHARS?(preview.substring(0, MAX_PREVIEW_CHARS)+'...'):preview+(imagelessLines.length > MAX_PREVIEW_LINES?'...':''))
--- ui.guscrawford.com/src/app/shared/markdown/markdown.service.ts
+++ ui.guscrawford.com/src/app/shared/markdown/markdown.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
-import { markdown } from 'markdown';
+import * as marked from 'marked';
 @Injectable()
 export class MarkdownService {
 
-  constructor() { }
-
+  constructor() {
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: false,
+      smartLists: true,
+      smartypants: false,
+      xhtml: false
+    });
+  }
+  renderer:any;
   fromMarkdown(md:string) {
     if (md)
-      return markdown.toHTML(md);
+      return marked(md);
     return '';
   }
 }
